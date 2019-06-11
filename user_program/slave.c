9a4f123903540fc39958a39b8e9a43b87b920b8d
DarrenLiou <fytar9963@gmail.com>
2019-06-11 22:13:42 +0800

    slave.c complete

--- user_program/slave.c
+++ user_program/slave.c
@@ -11,6 +11,7 @@
 
 #define PAGE_SIZE 4096
 #define BUF_SIZE 512
+typedef long long ll;
 int main (int argc, char* argv[])
 {
 	char buf[BUF_SIZE];
@@ -23,6 +24,8 @@ int main (int argc, char* argv[])
 	struct timeval end;
 	double trans_time; //calulate the time between the device is opened and it is closed
 	char *kernel_address, *file_address;
+    size_t pagenum=0;
+    char *mmapaddr=NULL;
 
 
 	strcpy(file_name, argv[1]);
@@ -46,7 +49,7 @@ int main (int argc, char* argv[])
 		perror("ioclt create slave socket error\n");
 		return 1;
 	}
-
+// we can comment this line
     write(1, "ioctl success\n", 14);
 
 	switch(method[0])
@@ -59,6 +62,44 @@ int main (int argc, char* argv[])
 				file_size += ret;
 			}while(ret > 0);
 			break;
+        case 'm': //mmap
+            while((ret=read(dev_fd,buf,sizeof(buf)))>0){
+                size_t remain = 0;
+                if((ll)PAGE_SIZE-(ll)ret-(ll)data_size <0 ||data_size<0){
+                    if(data_size<0){
+                        remain = PAGE_SIZE - data_size;
+                        pagenum++;
+                        for(size_t i=0; i<remain; i++){
+                            mmapaddr[data_size+i] = buf[i];
+                        }
+                        if(msync(mmapaddr,PAGE_SIZE,MS_ASYNC)==-1)
+                            perror("Can't sync to disk :(");
+                        if(munmap(mmapaddr,PAGE_SIZE)==-1)
+                            perror("Failed when unmmap!!");
+                    }
+                    if(lseek(file_fd,PAGE_SIZE-1,SEEK_END)==-1)
+                        perror("lseek to end failed!!");
+                    if(write(file_fd,"",1)==-1)
+                        perror("failed when write to end!");
+                    mmapaddr = (char*)mmap(NULL, PAGE_SIZE, PROT_READ|PROT_WRITE, MAP_SHARED,file_fd,pagenum*PAGE_SIZE);
+                    if(mmapaddr==MAP_FAILED)
+                        perror("mmap failed!");
+                    data_size = 0;
+                }
+                for(size_t i =0; i<ret-remain; i++)
+                    mmapaddr[data_size+i] = buf[remain+i];
+                data_size += ret;
+                file_size += ret;
+            }
+            if(msync(mmapaddr,PAGE_SIZE,MS_ASYNC)==-1)
+                perror("Can't sync to disk :( ");
+            if(munmap(mmapaddr,PAGE_SIZE)==-1)
+                perror("Failed when unmmap!!");
+            if(ftruncate(file_fd,file_size)==-1)
+                perror("Fail to truncate!");
+
+
+
 	}
 
 
@@ -70,7 +111,7 @@ int main (int argc, char* argv[])
 	}
 	gettimeofday(&end, NULL);
 	trans_time = (end.tv_sec - start.tv_sec)*1000 + (end.tv_usec - start.tv_usec)*0.0001;
-	printf("Transmission time: %lf ms, File size: %d bytes\n", trans_time, file_size / 8);
+	printf("Transmission time: %lf ms, File size: %ld bytes\n", trans_time, file_size / 8);
 
 
 	close(file_fd);
